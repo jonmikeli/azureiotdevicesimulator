6f28998f67f97ea11106a19807b60d50de32120c
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2019-08-24 18:24:00 +0200

    AF review (unit message processing for latency tests)

--- sources/IoT.Simulator2/IoT.Simulator2.AF/IoTFunctions.cs
+++ sources/IoT.Simulator2/IoT.Simulator2.AF/IoTFunctions.cs
@@ -4,8 +4,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,59 +12,46 @@ namespace IoT.Simulator2.Latency
     public static class IoTFunctions
     {
         [FunctionName("IoTLatencyTestHandler")]
-        public static async Task Run([EventHubTrigger("latency", Connection = "eventHubListenConnectionString")] EventData[] events, ILogger log)
+        public static async Task Run([EventHubTrigger("latency", Connection = "eventHubListenConnectionString")] EventData eventData, ILogger log)
         {
 
-            if (events != null && events.Count() > 0)
+            if (eventData != null)
             {
-                var exceptions = new List<Exception>();
-
                 string iotHubConnectionString = "[IoT Hub Owner connection string to be allowed to call C2D operations]";
                 ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(iotHubConnectionString);
 
                 if (serviceClient == null)
                     throw new Exception("Service client is null");
 
-                //NOTE: foreach could even be parallelized to improve performance
-                foreach (EventData eventData in events)
+                try
                 {
-                    try
-                    {
-                        string myIoTHubMessage = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                    string myIoTHubMessage = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
 
-                        if (string.IsNullOrEmpty(myIoTHubMessage))
-                            throw new ArgumentNullException(nameof(myIoTHubMessage));
+                    if (string.IsNullOrEmpty(myIoTHubMessage))
+                        throw new ArgumentNullException(nameof(myIoTHubMessage));
 
-                        LatencyTestRequest latencyTestRequest = JsonConvert.DeserializeObject<LatencyTestRequest>(myIoTHubMessage);
+                    LatencyTestRequest latencyTestRequest = JsonConvert.DeserializeObject<LatencyTestRequest>(myIoTHubMessage);
 
-                        if (latencyTestRequest == null)
-                            throw new ArgumentException("No JObject has been found in the request.", nameof(latencyTestRequest));
+                    if (latencyTestRequest == null)
+                        throw new ArgumentException("No JObject has been found in the request.", nameof(latencyTestRequest));
 
-                        var methodInvocation = new CloudToDeviceMethod("LatencyTestCallback") { ResponseTimeout = TimeSpan.FromSeconds(30) };
-                        methodInvocation.SetPayloadJson(latencyTestRequest.StartTimestamp.ToString());
+                    //NOTE: the Direct Method call could generalized if the request contains the name of the Direct Method that needs to be called.
+                    var methodInvocation = new CloudToDeviceMethod("LatencyTestCallback") { ResponseTimeout = TimeSpan.FromSeconds(30) };
+                    methodInvocation.SetPayloadJson(latencyTestRequest.StartTimestamp.ToString());
 
-                        // Invoke the direct method asynchronously and get the response from the simulated device.
-                        var response = await serviceClient.InvokeDeviceMethodAsync(latencyTestRequest.DeviceId, methodInvocation);
+                    // Invoke the direct method asynchronously and get the response from the simulated device.
+                    var response = await serviceClient.InvokeDeviceMethodAsync(latencyTestRequest.DeviceId, methodInvocation);
 
-                        log.LogDebug($"LATENCY: C2D message sent");
+                    log.LogDebug($"LATENCY: C2D message sent");
 
-                        await Task.Yield();
-                    }
-                    catch (Exception e)
-                    {
-                        // We need to keep processing the rest of the batch - capture this exception and continue.
-                        // Also, consider capturing details of the message that failed processing so it can be processed again later.
-                        log.LogError(e.Message);
-                        exceptions.Add(e);
-                    }
+                    await Task.Yield();
                 }
-
-                // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
-                if (exceptions.Count > 1)
-                    throw new AggregateException(exceptions);
-
-                if (exceptions.Count == 1)
-                    throw exceptions.Single();
+                catch (Exception e)
+                {
+                    // We need to keep processing the rest of the batch - capture this exception and continue.
+                    // Also, consider capturing details of the message that failed processing so it can be processed again later.
+                    log.LogError(e.Message);
+                }
             }
         }
 
