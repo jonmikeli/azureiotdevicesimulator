ea55882d38ab3923e7662469633360d305b40511
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2019-08-24 13:40:39 +0200

    few code updates

--- sources/IoT.Simulator2/IoT.Simulator2.AF/IoTFunctions.cs
+++ sources/IoT.Simulator2/IoT.Simulator2.AF/IoTFunctions.cs
@@ -1,14 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Azure.Devices;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace IoT.Simulator2.Latency
 {
@@ -17,55 +16,58 @@ namespace IoT.Simulator2.Latency
         [FunctionName("IoTLatencyTestHandler")]
         public static async Task Run([EventHubTrigger("latency", Connection = "eventHubListenConnectionString")] EventData[] events, ILogger log)
         {
-            var exceptions = new List<Exception>();
 
-            foreach (EventData eventData in events)
+            if (events != null && events.Count() > 0)
             {
-                try
+                var exceptions = new List<Exception>();
+
+                string iotHubConnectionString = "[IoT Hub Owner connection string to be allowed to call C2D operations]";
+                ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(iotHubConnectionString);
+
+                if (serviceClient == null)
+                    throw new Exception("Service client is null");
+
+                //NOTE: foreach could even be parallelized to improve performance
+                foreach (EventData eventData in events)
                 {
-                    string myIoTHubMessage = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                    try
+                    {
+                        string myIoTHubMessage = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
 
-                    if (string.IsNullOrEmpty(myIoTHubMessage))
-                        throw new ArgumentNullException(nameof(myIoTHubMessage));
+                        if (string.IsNullOrEmpty(myIoTHubMessage))
+                            throw new ArgumentNullException(nameof(myIoTHubMessage));
 
-                    LatencyTestRequest latencyTestRequest = JsonConvert.DeserializeObject<LatencyTestRequest>(myIoTHubMessage);
+                        LatencyTestRequest latencyTestRequest = JsonConvert.DeserializeObject<LatencyTestRequest>(myIoTHubMessage);
 
-                    if (latencyTestRequest == null)
-                        throw new ArgumentException("No JObject has been found in the request.", nameof(latencyTestRequest));
+                        if (latencyTestRequest == null)
+                            throw new ArgumentException("No JObject has been found in the request.", nameof(latencyTestRequest));
 
-                    //string iotHubConnectionString = "[IoT Hub Owner connection string to be allowed to call C2D operations]";
-                    string iotHubConnectionString = "HostName=iotedgedev-iothub-d62a46.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=K0YH3iPMzkphkgKyakRWf32mc75T14JJxEGbgQ2jsfU=";
-                    ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(iotHubConnectionString);
+                        var methodInvocation = new CloudToDeviceMethod("LatencyTestCallback") { ResponseTimeout = TimeSpan.FromSeconds(30) };
+                        methodInvocation.SetPayloadJson(latencyTestRequest.StartTimestamp.ToString());
 
-                    if (serviceClient == null)
-                        throw new Exception("Service client is null");
+                        // Invoke the direct method asynchronously and get the response from the simulated device.
+                        var response = await serviceClient.InvokeDeviceMethodAsync(latencyTestRequest.DeviceId, methodInvocation);
 
-                    var methodInvocation = new CloudToDeviceMethod("LatencyTestCallback") { ResponseTimeout = TimeSpan.FromSeconds(30) };
-                    methodInvocation.SetPayloadJson(latencyTestRequest.StartTimestamp.ToString());
+                        log.LogDebug($"LATENCY: C2D message sent");
 
-                    // Invoke the direct method asynchronously and get the response from the simulated device.
-                    var response = await serviceClient.InvokeDeviceMethodAsync(latencyTestRequest.DeviceId, methodInvocation);
-
-                    log.LogDebug($"LATENCY: C2D message sent");
-
-                    await Task.Yield();
+                        await Task.Yield();
+                    }
+                    catch (Exception e)
+                    {
+                        // We need to keep processing the rest of the batch - capture this exception and continue.
+                        // Also, consider capturing details of the message that failed processing so it can be processed again later.
+                        log.LogError(e.Message);
+                        exceptions.Add(e);
+                    }
                 }
-                catch (Exception e)
-                {
-                    // We need to keep processing the rest of the batch - capture this exception and continue.
-                    // Also, consider capturing details of the message that failed processing so it can be processed again later.
-                    log.LogError(e.Message);
-                    exceptions.Add(e);
-                }
+
+                // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
+                if (exceptions.Count > 1)
+                    throw new AggregateException(exceptions);
+
+                if (exceptions.Count == 1)
+                    throw exceptions.Single();
             }
-
-            // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
-
-            if (exceptions.Count > 1)
-                throw new AggregateException(exceptions);
-
-            if (exceptions.Count == 1)
-                throw exceptions.Single();
         }
 
         private static long ConvertToTimeStamp(DateTime data)
